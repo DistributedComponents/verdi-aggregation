9bedf74858e156ee840d16d1c392f312504a58d9
Karl Palmskog <palmskog@gmail.com>
2016-01-03 21:58:24 -0600

    added type class for partial mapping and simulation theorem for step_m

--- core/Simul.v
+++ core/Simul.v
@@ -37,6 +37,23 @@ Class MultiParamsTotMap
    tot_map_name_inv : @name B1 P1 -> @name B0 P0
 }.
 
+Class BaseParamsPtMap (P0 : BaseParams) (P1 : BaseParams) := 
+  {
+    pt_map_data : @data P0 -> @data P1 ;
+    pt_map_input : @input P0 -> option (@input P1) ;
+    pt_map_output : @output P0 -> option (@output P1)
+  }.
+
+Class MultiParamsPtMap
+ (B0 : BaseParams) (B1 : BaseParams) 
+ (B : BaseParamsPtMap B0 B1)
+ (P0 : MultiParams B0) (P1 : MultiParams B1)  :=
+{
+   pt_map_msg : @msg B0 P0 -> option (@msg B1 P1) ;
+   pt_map_name : @name B0 P0 -> @name B1 P1 ;
+   pt_map_name_inv : @name B1 P1 -> @name B0 P0
+}.
+
 Lemma map_eq_inv :
   forall (A B : Type) (f : A -> B) (l : list A) xs ys,
     map f l = xs ++ ys ->
@@ -907,3 +924,366 @@ invcs H_step.
 Qed.
 
 End SimulTot.
+
+Section SimulPt.
+
+Context {base_fst : BaseParams}.
+Context {base_snd : BaseParams}.
+Context {multi_fst : MultiParams base_fst}.
+Context {multi_snd : MultiParams base_snd}.
+Context {base_map : BaseParamsPtMap base_fst base_snd}.
+Context {multi_map : MultiParamsPtMap base_map multi_fst multi_snd}.
+
+Hypothesis pt_map_name_inv_inverse : forall n, pt_map_name_inv (pt_map_name n) = n.
+
+Hypothesis pt_map_name_inverse_inv : forall n, pt_map_name (pt_map_name_inv n) = n.
+
+Hypothesis pt_init_handlers_eq : forall n,
+  pt_map_data (init_handlers n) = init_handlers (pt_map_name n).
+
+Definition pt_map_name_msgs :=
+  fold_right (fun nm l => 
+                match pt_map_msg (snd nm) with
+                | Some m => (pt_map_name (fst nm), m) :: l
+                | None => l
+                end) [].
+
+Definition pt_map_outputs :=
+  fold_right (fun o l =>
+                match pt_map_output o with
+                | Some o' => o' :: l
+                | None => l
+                end) [].
+
+Definition pt_mapped_net_handlers me src m st :=
+  let '(out, st', ps) := net_handlers me src m st in
+  (pt_map_outputs out, pt_map_data st', pt_map_name_msgs ps).
+
+Hypothesis pt_net_handlers_some : forall me src m st m',
+  pt_map_msg m = Some m' ->
+  pt_mapped_net_handlers me src m st = net_handlers (pt_map_name me) (pt_map_name src) m' (pt_map_data st).
+
+Hypothesis pt_net_handlers_none : forall me src m st out st' ps,
+  pt_map_msg m = None ->
+  net_handlers me src m st = (out, st', ps) ->
+  pt_map_data st' = pt_map_data st /\ pt_map_name_msgs ps = [] /\ pt_map_outputs out = [].
+
+Definition pt_mapped_input_handlers me inp st :=
+  let '(out, st', ps) := input_handlers me inp st in
+  (pt_map_outputs out, pt_map_data st', pt_map_name_msgs ps).
+
+Hypothesis pt_input_handlers_some : forall me inp st inp',
+  pt_map_input inp = Some inp' ->
+  pt_mapped_input_handlers me inp st = input_handlers (pt_map_name me) inp' (pt_map_data st).
+
+Hypothesis pt_input_handlers_none : forall me inp st out st' ps,
+  pt_map_input inp = None ->
+  input_handlers me inp st = (out, st', ps) ->
+  pt_map_data st' = pt_map_data st /\ pt_map_name_msgs ps = [] /\ pt_map_outputs out = [].
+
+Definition pt_map_trace_occ (e : @name _ multi_fst * (@input base_fst + list (@output base_fst))) :
+ option (@name _ multi_snd * (@input base_snd + list (@output base_snd))) :=
+match e with
+| (n, inl io) => 
+  match pt_map_input io with
+  | Some io' => Some (pt_map_name n, inl io')
+  | None => None
+  end
+| (n, inr out) => Some (pt_map_name n, inr (pt_map_outputs out))
+end.
+
+Definition pt_map_trace :=
+fold_right (fun e l =>
+              match pt_map_trace_occ e with
+              | Some e' => e' :: l
+              | None => l
+              end) [].
+
+Definition pt_map_packet (p : @packet base_fst multi_fst)  :=
+match p with
+| mkPacket src dst m =>
+  match pt_map_msg m with
+  | Some m' => Some (mkPacket (pt_map_name src) (pt_map_name dst) m')
+  | None => None
+  end
+end.
+
+Definition pt_map_packets :=
+fold_right (fun p l =>
+            match pt_map_packet p with
+            | Some p' => p' :: l
+            | None => l
+            end) [].
+
+Definition pt_map_net (net : @network _ multi_fst) : @network _ multi_snd :=
+mkNetwork (pt_map_packets net.(nwPackets)) (fun n => pt_map_data (net.(nwState) (pt_map_name_inv n))).
+
+Lemma pt_init_handlers_fun_eq : 
+    init_handlers = fun n : name => pt_map_data (init_handlers (pt_map_name_inv n)).
+Proof.
+apply functional_extensionality => n.
+have H_eq := pt_init_handlers_eq.
+rewrite H_eq {H_eq}.
+by rewrite pt_map_name_inverse_inv.
+Qed.
+
+Lemma pt_map_name_msgs_app_distr : 
+  forall l l',
+  pt_map_name_msgs (l ++ l') = pt_map_name_msgs l ++ pt_map_name_msgs l'.
+Proof.
+elim => //=.
+case => n m l IH l'.
+rewrite /= IH.
+by case (pt_map_msg _) => [m'|].
+Qed.
+
+Lemma pt_map_packets_app_distr : 
+  forall l l',
+  pt_map_packets (l ++ l') = pt_map_packets l ++ pt_map_packets l'.
+Proof.
+elim => //=.
+move => n l IH l'.
+rewrite /= IH.
+by case (pt_map_packet _).
+Qed.
+
+Lemma pt_map_name_msgs_empty_eq :
+  forall l dst,
+  pt_map_name_msgs l = [] ->
+  pt_map_packets (map (fun m0 : name * msg => {| pSrc := dst; pDst := fst m0; pBody := snd m0 |}) l) = [].
+Proof.
+elim => //=.
+case => n m l IH dst.
+case H_m: (pt_map_msg _) => [m'|] //=.
+move => H_eq.
+by rewrite IH.
+Qed.
+
+Lemma pt_map_packet_map_app_eq :
+  forall l h ms,
+    pt_map_packets (map (fun m : name * msg => {| pSrc := h; pDst := fst m; pBody := snd m |}) l ++ ms) = 
+    map (fun m : name * msg => {| pSrc := pt_map_name h; pDst := fst m; pBody := snd m |}) (pt_map_name_msgs l) ++ pt_map_packets ms.
+Proof.
+move => l h ms.
+elim: l => //=.
+case => n m l IH.
+case (pt_map_msg _) => [m'|] //.
+by rewrite IH.
+Qed.
+
+Lemma pt_map_packet_app_eq :
+  forall l p p' ms ms',
+    pt_map_packet p = Some p' ->
+    pt_map_packets (map (fun m : name * msg => {| pSrc := pDst p; pDst := fst m; pBody := snd m |}) l ++ ms ++ ms') = 
+    map (fun m : name * msg => {| pSrc := pDst p'; pDst := fst m; pBody := snd m |}) (pt_map_name_msgs l) ++ pt_map_packets ms ++ pt_map_packets ms'.
+Proof.
+move => l; case => /= src dst m p ms ms'.
+case H_m: (pt_map_msg m) => [m'|] // H_eq.
+injection H_eq => H_eq_p.
+rewrite -H_eq_p /=.
+rewrite -pt_map_packets_app_distr.
+exact: pt_map_packet_map_app_eq.
+Qed.
+
+Lemma pt_map_update_eq :
+forall net h d,
+  (fun n : name => pt_map_data (update (nwState net) h d (pt_map_name_inv n))) =
+  update (fun n : name => pt_map_data (nwState net (pt_map_name_inv n))) (pt_map_name h) (pt_map_data d).
+Proof.
+move => net h d.
+apply functional_extensionality => n.
+rewrite /update /=.
+case (name_eq_dec _ _) => H_dec; case (name_eq_dec _ _) => H_dec' //.
+  rewrite -H_dec in H_dec'.
+  by rewrite pt_map_name_inverse_inv in H_dec'.
+rewrite H_dec' in H_dec.
+by rewrite pt_map_name_inv_inverse in H_dec.
+Qed.
+
+Lemma pt_map_update_eq_some :
+  forall net d p p',
+    pt_map_packet p = Some p' ->
+    (fun n : name => pt_map_data (update (nwState net) (pDst p) d (pt_map_name_inv n))) =
+    update (fun n : name => pt_map_data (nwState net (pt_map_name_inv n))) (pDst p') (pt_map_data d).
+Proof.
+move => net d p p'.
+case: p => src dst m.
+case: p' => src' dst' m' /=.
+case H_eq: (pt_map_msg _) => [m0|] // H_eq'.
+inversion H_eq'; subst.
+move {H_eq H_eq'}.
+exact: pt_map_update_eq.
+Qed.
+
+Definition pt_trace_remove_empty_out :=
+  fold_right (fun (e : @name _ multi_snd * (@input base_snd + list (@output base_snd))) l => 
+                match e with
+                | (n, inr []) => l
+                | _ => e :: l
+                end) [].
+
+Theorem step_m_pt_mapped_simulation_1 :
+  forall net net' tr,
+    @step_m _ multi_fst net net' tr ->
+    @step_m _ multi_snd (pt_map_net net) (pt_map_net net') (pt_map_trace tr) \/ 
+    (pt_map_net net' = pt_map_net net /\ pt_trace_remove_empty_out (pt_map_trace tr) = []).
+Proof.
+move => net net' tr.
+case => {net net' tr}.
+- move => net net' p ms ms' out d l H_eq H_hnd H_eq'.
+  rewrite /pt_map_trace /=.  
+  case H_m: (pt_map_packet p) => [p'|].
+    have ->: pt_map_name (pDst p) = pDst p'.
+      case: p H_eq H_hnd H_eq' H_m => /= src dst m H_eq H_hnd H_eq'.
+      case (pt_map_msg m) => //= m' H_m.
+      by inversion H_m.
+    left.
+    rewrite H_eq' /=.
+    apply (@SM_deliver _ _ _ _ _ (pt_map_packets ms) (pt_map_packets ms') (pt_map_outputs out) (pt_map_data d) (pt_map_name_msgs l)).
+    * rewrite /= H_eq pt_map_packets_app_distr /=.
+      case H_p: (pt_map_packet _) => [p0|].
+        rewrite H_p in H_m.
+        by injection H_m => H_eq_p; rewrite H_eq_p.
+      by rewrite H_p in H_m.
+    * rewrite /=.
+      case: p H_eq H_hnd H_eq' H_m => /= src dst m H_eq H_hnd H_eq'.
+      case H_m: (pt_map_msg m) => [m'|] //.
+      case: p' H_eq' => src' dst' m0 H_eq' H_eq_p.
+      inversion H_eq_p; subst.
+      rewrite /= {H_eq_p}.
+      have H_q := pt_net_handlers_some dst src m (nwState net dst) H_m.
+      rewrite /pt_mapped_net_handlers in H_q.
+      rewrite H_hnd in H_q.
+      rewrite H_q.
+      by rewrite pt_map_name_inv_inverse.
+    * rewrite /= /pt_map_net /=.
+      rewrite (pt_map_packet_app_eq _ _ _ _ H_m).
+      by rewrite (pt_map_update_eq_some _ _ _ H_m).
+  right.
+  split.
+  * rewrite H_eq' {H_eq'}.
+    rewrite /pt_map_net /=.
+    case: p H_eq H_hnd H_m => /= src dst m H_eq H_hnd.
+    case H_m: (pt_map_msg _) => [m'|] // H_eq'.
+    rewrite 2!pt_map_packets_app_distr H_eq pt_map_packets_app_distr /=.
+    case H_m': (pt_map_msg _) => [m'|]; first by rewrite H_m' in H_m.
+    have [H_d [H_l H_o]] := pt_net_handlers_none _ _ _ _ H_m H_hnd.
+    rewrite (pt_map_name_msgs_empty_eq _ dst H_l) /=.
+    set nwS1 := fun _ => _.
+    set nwS2 := fun _ => _.
+    have H_eq_s: nwS1 = nwS2.
+      rewrite /nwS1 /nwS2 /=.
+      apply functional_extensionality => n.
+      rewrite /update /=.
+      case (name_eq_dec _ _) => H_dec //.
+      by rewrite H_dec H_d.
+    by rewrite H_eq_s.
+  * move {H_eq'}.
+    case: p H_eq H_hnd H_m => /= src dst m H_eq H_hnd.
+    case H_m: (pt_map_msg _) => [m'|] // H_eq'.
+    have [H_d [H_l H_o]] := pt_net_handlers_none _ _ _ _ H_m H_hnd.
+    by rewrite H_o.
+- move => h net net' out inp d l H_hnd H_eq.
+  rewrite /pt_map_trace /=.  
+  case H_i: (pt_map_input inp) => [inp'|].
+    left.
+    apply (@SM_input _ _ _ _ _ _ _ (pt_map_data d) (pt_map_name_msgs l)).
+      rewrite /=.
+      have H_q := pt_input_handlers_some h inp (nwState net h) H_i.
+      rewrite /pt_mapped_input_handlers /= in H_q.
+      rewrite H_hnd in H_q.
+      rewrite H_q.
+      by rewrite pt_map_name_inv_inverse.
+    rewrite /= H_eq /= /pt_map_net /=.  
+    rewrite pt_map_packet_map_app_eq.
+    by rewrite -pt_map_update_eq.
+  right.
+  split.
+  * rewrite H_eq /pt_map_net /=.
+    have [H_d [H_l H_o]] := pt_input_handlers_none _ _ _ H_i H_hnd.
+    rewrite pt_map_packets_app_distr.
+    rewrite (pt_map_name_msgs_empty_eq _ h H_l) /=.
+    set nwS1 := fun _ => _.
+    set nwS2 := fun _ => _.
+    have H_eq_s: nwS1 = nwS2.
+      rewrite /nwS1 /nwS2 /=.
+      apply functional_extensionality => n.
+      rewrite /update /=.
+      case (name_eq_dec _ _) => H_dec //.
+      by rewrite H_dec H_d.
+    by rewrite H_eq_s.
+  * rewrite /=.
+    have [H_d [H_l H_o]] := pt_input_handlers_none _ _ _ H_i H_hnd.
+    by rewrite H_o.
+Qed.
+
+Lemma pt_map_trace_app_distr : 
+  forall tr1 tr2,
+  pt_map_trace (tr1 ++ tr2) = pt_map_trace tr1 ++ pt_map_trace tr2.
+Proof.
+elim => //.
+case => n.
+case.
+  move => inp l IH tr2.
+  rewrite /=.
+  case (pt_map_input _) => [io'|] //. 
+  by rewrite IH.
+move => out l IH tr2.
+rewrite /=.
+by rewrite IH.
+Qed.
+
+Lemma pt_trace_remove_empty_out_app_distr :
+  forall tr1 tr2,
+    pt_trace_remove_empty_out (tr1 ++ tr2 ) = pt_trace_remove_empty_out tr1 ++ pt_trace_remove_empty_out tr2.
+Proof.
+elim => //.
+case => n.
+case.
+  move => inp l IH tr2.
+  by rewrite /= IH.
+move => out l IH tr2.
+rewrite /= IH.
+by case: out.
+Qed.
+
+Corollary step_m_pt_mapped_simulation_star_1 :
+  forall net tr,
+    @step_m_star _ multi_fst step_m_init net tr ->
+    exists tr', @step_m_star _ multi_snd step_m_init (pt_map_net net) tr' /\ 
+     pt_trace_remove_empty_out (pt_map_trace tr) = pt_trace_remove_empty_out tr'.
+Proof.
+move => net tr H_step.
+remember step_m_init as y in *.
+move: Heqy.
+induction H_step using refl_trans_1n_trace_n1_ind => H_init /=.
+  rewrite H_init.
+  rewrite /step_m_init /= /pt_map_net /=.
+  rewrite pt_init_handlers_fun_eq.
+  exists [].
+  split => //.
+  exact: RT1nTBase.
+concludes.
+rewrite H_init in H_step2 H_step1.
+apply step_m_pt_mapped_simulation_1 in H.
+case: H => H.
+  move: IHH_step1 => [tr' [H_star H_eq_tr]].
+  exists (tr' ++ pt_map_trace tr2).
+  split.
+  * have H_trans := refl_trans_1n_trace_trans H_star.
+    apply: H_trans.
+    rewrite (app_nil_end (pt_map_trace _)).
+    apply: (@RT1nTStep _ _ _ _ (pt_map_net x'')) => //.
+    exact: RT1nTBase.
+  * rewrite pt_map_trace_app_distr pt_trace_remove_empty_out_app_distr H_eq_tr.
+    by rewrite pt_trace_remove_empty_out_app_distr.
+move: H => [H_eq H_eq'].
+rewrite H_eq.
+move: IHH_step1 => [tr' [H_star H_tr]].
+exists tr'.
+split => //.
+rewrite pt_map_trace_app_distr pt_trace_remove_empty_out_app_distr.
+by rewrite H_eq' -app_nil_end.
+Qed.
+
+End SimulPt.
