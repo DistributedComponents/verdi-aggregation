3c4f9c3813f33b18e41ec9baf0209ce071054be0
Karl Palmskog <palmskog@gmail.com>
2016-06-08 02:10:02 -0500

    improve liveness definitions

--- core/LabeledNet.v
+++ core/LabeledNet.v
@@ -15,10 +15,10 @@ Class LabeledMultiParams (P : BaseParams) :=
     lb_all_names_nodes : forall n, In n lb_nodes ;
     lb_no_dup_nodes : NoDup lb_nodes ;
     label : Type ;
-    label_stutter : label ;
+    label_silent : label ;
     lb_init_handlers : lb_name -> data ;
-    lb_net_handlers : lb_name -> lb_name -> lb_msg -> data -> (list output) * data * list (lb_name * lb_msg) * label ;
-    lb_input_handlers : lb_name -> input -> data -> (list output) * data * list (lb_name * lb_msg) * label
+    lb_net_handlers : lb_name -> lb_name -> lb_msg -> data -> label * (list output) * data * list (lb_name * lb_msg) ;
+    lb_input_handlers : lb_name -> input -> data -> label * (list output) * data * list (lb_name * lb_msg)
   }.
 
 Section LabeledParams.
@@ -27,10 +27,10 @@ Context {base_params : BaseParams}.
 Context {params : LabeledMultiParams base_params}.
 
 Definition unlabeled_net_handlers me src m st :=
-let '(out, st', ps, lb) := lb_net_handlers me src m st in (out, st', ps).
+let '(lb, out, st', ps) := lb_net_handlers me src m st in (out, st', ps).
 
 Definition unlabeled_input_handlers me inp st :=
-let '(out, st', ps, lb) := lb_input_handlers me inp st in (out, st', ps).
+let '(lb, out, st', ps) := lb_input_handlers me inp st in (out, st', ps).
 
 Global Instance unlabeled_multi_params : MultiParams base_params :=
   {
@@ -105,41 +105,44 @@ Section LabeledStepRelations.
   Qed.
 End LabeledStepRelations.
 
-Section LabeledStepAsync.
-
+Section LabeledStepFailure.
   Context `{params : LabeledMultiParams}.
 
-  Inductive lb_step_m : lb_step_relation network label (name * (input + list output)) :=
-  | LSM_deliver : forall net net' p xs ys out d l lb,
+  Inductive lb_step_f : lb_step_relation (list name * network) label (name * (input + list output)) :=
+  | LSF_deliver : forall net net' failed p xs ys out d l lb,
                      nwPackets net = xs ++ p :: ys ->
-                     lb_net_handlers (pDst p) (pSrc p) (pBody p) (nwState net (pDst p)) = (out, d, l, lb) ->
+                     ~ In (pDst p) failed ->
+                     lb_net_handlers (pDst p) (pSrc p) (pBody p) (nwState net (pDst p)) = (lb, out, d, l) ->
                      net' = mkNetwork (send_packets (pDst p) l ++ xs ++ ys)
                                       (update (nwState net) (pDst p) d) ->
-                     lb_step_m net lb net' [(pDst p, inr out)]
-  | LSM_input : forall h net net' out inp d l lb,
-                   lb_input_handlers h inp (nwState net h) = (out, d, l, lb) ->
-                   net' = mkNetwork (send_packets h l ++ nwPackets net)
-                                    (update (nwState net) h d) ->
-                   lb_step_m net lb net' [(h, inl inp); (h, inr out)]
-  | LSM_stutter : forall net, lb_step_m net label_stutter net [].
+                     lb_step_f (failed, net) lb (failed, net') [(pDst p, inr out)]
+  | LSF_input : forall h net net' failed out inp d l lb,
+                  ~ In h failed ->
+                  lb_input_handlers h inp (nwState net h) = (lb, out, d, l) ->
+                  net' = mkNetwork (send_packets h l ++ nwPackets net)
+                                   (update (nwState net) h d) ->
+                  lb_step_f (failed, net) lb (failed, net') [(h, inl inp); (h, inr out)]
+  | LSF_stutter : forall net failed, lb_step_f (failed, net) label_silent (failed, net) [].
+  
+  Context {failure_params : FailureParams unlabeled_multi_params}.
 
-  Definition event_step_m_star (e : event _ label) :=
-    exists tr, step_m_star step_m_init (a_of_event e) tr.
+  Definition event_step_f_star (e : event (list name * network) label) :=
+    exists tr, step_f_star step_f_init (a_of_event e) tr.
 
-  Lemma step_m_star_lb_step_reachable_ex :
-    forall net l net' tr tr',
-    lb_step_m net l net' tr ->
-    step_m_star step_m_init net tr' ->
-    exists tr0, step_m_star step_m_init net' tr0.
+  Lemma step_f_star_lb_step_reachable_ex :
+    forall net l net' failed failed' tr tr',
+    lb_step_f (failed, net) l (failed', net') tr ->
+    step_f_star step_f_init (failed, net) tr' ->
+    exists tr0, step_f_star step_f_init (failed', net') tr0.
   Proof.  
-  move => net l net' tr tr' H_step H_star.
-  exists (tr ++ tr').    
+  move => net l net' failed failed' tr tr' H_step H_star.
+  exists (tr ++ tr').
   Admitted.
 
-  Lemma step_m_star_lb_step_execution : 
-    forall s, event_step_m_star (hd s) ->
-         lb_step_execution lb_step_m s ->
-         always (now event_step_m_star) s.
+  Lemma step_f_star_lb_step_execution :
+    forall s, event_step_f_star (hd s) ->
+         lb_step_execution lb_step_f s ->
+         always (now event_step_f_star) s.
   Proof.
   case => e s H_star.
   move: e s H_star.
@@ -149,60 +152,138 @@ Section LabeledStepAsync.
   constructor; first by [].
   apply cf.
     inversion H_exec'; subst_max.
-    rewrite /event_step_m_star /a_of_event /=.
-    rewrite /event_step_m_star /a_of_event /= in H_star.
+    rewrite /event_step_f_star /a_of_event /=.
+    rewrite /event_step_f_star /a_of_event /= in H_star.
     break_exists.
     move: H1 H.
-    apply: step_m_star_lb_step_reachable_ex.
+    destruct a, a'.
+    apply step_f_star_lb_step_reachable_ex.
   move: H_exec'.
   exact: lb_step_execution_invar.
-  Qed.      
-End LabeledStepAsync.
+  Qed.
+End LabeledStepFailure.
 
 Section LabeledStepOrder.
   Context `{params : LabeledMultiParams}.
 
-  Inductive lb_step_o : lb_step_relation (list name * ordered_network) label (name * (input + list output)) :=
-  | LSO_deliver : forall net net' failed m ms out d l from to lb,
+  Inductive lb_step_o_f : lb_step_relation (list name * ordered_network) label (name * (input + list output)) :=
+  | LSOF_deliver : forall net net' failed m ms out d l from to lb,
                      onwPackets net from to = m :: ms ->
                      ~ In to failed ->
-                     lb_net_handlers to from m (onwState net to) = (out, d, l, lb) ->
+                     lb_net_handlers to from m (onwState net to) = (lb, out, d, l) ->
                      net' = mkONetwork (collate to (update2 (onwPackets net) from to ms) l)
                                        (update' (onwState net) to d) ->
-                     lb_step_o (failed, net) lb (failed, net') [(to, inr out)]
-  | LSO_input : forall h net net' failed out inp d l lb,
+                     lb_step_o_f (failed, net) lb (failed, net') [(to, inr out)]
+  | LSOF_input : forall h net net' failed out inp d l lb,
                    ~ In h failed ->
-                   lb_input_handlers h inp (onwState net h) = (out, d, l, lb) ->
+                   lb_input_handlers h inp (onwState net h) = (lb, out, d, l) ->
                    net' = mkONetwork (@collate name EqDec_eq_name msg h (onwPackets net) l)
                                      (update' (onwState net) h d) ->
-                   lb_step_o (failed, net) lb (failed, net') [(h, inl inp); (h, inr out)]
-  | LSO_stutter : forall net failed, lb_step_o (failed, net) label_stutter (failed, net) [].
+                   lb_step_o_f (failed, net) lb (failed, net') [(h, inl inp); (h, inr out)]
+  | LSOF_stutter : forall net failed, lb_step_o_f (failed, net) label_silent (failed, net) [].
 
+  Context {overlay_params : NameOverlayParams unlabeled_multi_params}.
+  Context {fail_msg_params : FailMsgParams unlabeled_multi_params}.
+  
+  Definition event_step_o_f_star (e : event (list name * ordered_network) label) :=
+    exists tr, step_o_f_star step_o_f_init (a_of_event e) tr.
+
+  Lemma step_o_f_star_lb_step_reachable_ex :
+    forall net l net' failed failed' tr tr',
+    lb_step_o_f (failed, net) l (failed', net') tr ->
+    step_o_f_star step_o_f_init (failed, net) tr' ->
+    exists tr0, step_o_f_star step_o_f_init (failed', net') tr0.
+  Proof.
+  move => net l net' failed failed' tr tr' H_step H_star.
+  exists (tr ++ tr').
+  Admitted.
+
+  Lemma step_o_f_star_lb_step_execution :
+    forall s, event_step_o_f_star (hd s) ->
+         lb_step_execution lb_step_o_f s ->
+         always (now event_step_o_f_star) s.
+  Proof.
+  case => e s H_star.
+  move: e s H_star.
+  cofix cf.
+  move => e.
+  case => e' s H_star H_exec'.
+  constructor; first by [].
+  apply cf.
+    inversion H_exec'; subst_max.
+    rewrite /event_step_o_f_star /a_of_event /=.
+    rewrite /event_step_o_f_star /a_of_event /= in H_star.
+    break_exists.
+    move: H1 H.
+    destruct a, a'.
+    apply step_o_f_star_lb_step_reachable_ex.
+  move: H_exec'.
+  exact: lb_step_execution_invar.
+  Qed.
 End LabeledStepOrder.
 
 Section LabeledStepOrderDynamic.
   Context `{params : LabeledMultiParams}.
 
-  Inductive lb_step_o_d : lb_step_relation (list name * ordered_dynamic_network) label (name * (input + list output)) :=
-  | LSOD_deliver : forall net net' failed m ms out d d' l from to lb,
+  Inductive lb_step_o_d_f : lb_step_relation (list name * ordered_dynamic_network) label (name * (input + list output)) :=
+  | LSODF_deliver : forall net net' failed m ms out d d' l from to lb,
       ~ In to failed ->
       In to (odnwNodes net) ->
       odnwState net to = Some d ->
       odnwPackets net from to = m :: ms ->
-      lb_net_handlers to from m d = (out, d', l, lb) ->
+      lb_net_handlers to from m d = (lb, out, d', l) ->
       net' = {| odnwNodes := odnwNodes net;
                 odnwPackets := collate to (update2 (odnwPackets net) from to ms) l;
                 odnwState := update_opt (odnwState net) to d' |} ->
-      lb_step_o_d (failed, net) lb (failed, net') [(to, inr out)]
-  | LSOD_input : forall h net net' failed out inp d d' l lb,
+      lb_step_o_d_f (failed, net) lb (failed, net') [(to, inr out)]
+  | LSODF_input : forall h net net' failed out inp d d' l lb,
       ~ In h failed ->
       In h (odnwNodes net) ->
       odnwState net h = Some d ->
-      lb_input_handlers h inp d = (out, d', l, lb) ->
+      lb_input_handlers h inp d = (lb, out, d', l) ->
       net' = {| odnwNodes := odnwNodes net;
                 odnwPackets := collate h (odnwPackets net) l;
                 odnwState := update_opt (odnwState net) h d' |} ->
-      lb_step_o_d (failed, net) lb (failed, net') [(h, inl inp); (h, inr out)]
-  | LSOD_stutter : forall net failed, lb_step_o_d (failed, net) label_stutter (failed, net) [].
+      lb_step_o_d_f (failed, net) lb (failed, net') [(h, inl inp); (h, inr out)]
+  | LSODF_stutter : forall net failed, lb_step_o_d_f (failed, net) label_silent (failed, net) [].
 
+  Context {overlay_params : NameOverlayParams unlabeled_multi_params}.
+  Context {fail_msg_params : FailMsgParams unlabeled_multi_params}.
+  Context {new_msg_params : NewMsgParams unlabeled_multi_params}.
+
+  Definition event_step_o_d_f_star (e : event (list name * ordered_dynamic_network) label) :=
+    exists tr, step_o_d_f_star step_o_d_f_init (a_of_event e) tr.
+
+  Lemma step_o_d_f_star_lb_step_reachable_ex :
+    forall net l net' failed failed' tr tr',
+    lb_step_o_d_f (failed, net) l (failed', net') tr ->
+    step_o_d_f_star step_o_d_f_init (failed, net) tr' ->
+    exists tr0, step_o_d_f_star step_o_d_f_init (failed', net') tr0.
+  Proof.
+  move => net l net' failed failed' tr tr' H_step H_star.
+  exists (tr ++ tr').
+  Admitted.
+
+  Lemma step_o_d_f_star_lb_step_execution :
+    forall s, event_step_o_d_f_star (hd s) ->
+         lb_step_execution lb_step_o_d_f s ->
+         always (now event_step_o_d_f_star) s.
+  Proof.
+  case => e s H_star.
+  move: e s H_star.
+  cofix cf.
+  move => e.
+  case => e' s H_star H_exec'.
+  constructor; first by [].
+  apply cf.
+    inversion H_exec'; subst_max.
+    rewrite /event_step_o_d_f_star /a_of_event /=.
+    rewrite /event_step_o_d_f_star /a_of_event /= in H_star.
+    break_exists.
+    move: H1 H.
+    destruct a, a'.
+    apply step_o_d_f_star_lb_step_reachable_ex.
+  move: H_exec'.
+  exact: lb_step_execution_invar.
+  Qed.
 End LabeledStepOrderDynamic.
