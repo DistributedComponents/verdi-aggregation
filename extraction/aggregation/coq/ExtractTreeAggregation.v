982560491a751ff5f686cd20e745d24378099a1b
Karl Palmskog <palmskog@gmail.com>
2016-07-11 21:42:17 -0500

    simple testing instructions for tree aggregation extraction

--- extraction/aggregation/coq/ExtractTreeAggregation.v
+++ extraction/aggregation/coq/ExtractTreeAggregation.v
@@ -58,7 +58,7 @@ Require Import FMapList.
 Module N5Map <: FMapInterface.S := FMapList.Make NOTC_N5.
 Module RNT_N5 := FinRootNameType N5 FN_N5.
 
-Module CFG : CommutativeFinGroup.
+Module CFG <: CommutativeFinGroup.
 Definition gT := [finGroupType of 'I_128].
 Lemma mulgC : @commutative gT _ mulg. exact: Zp_mulgC. Qed.
 End CFG.
@@ -66,4 +66,39 @@ End CFG.
 Module TA := TreeAggregation FN_N5 NOT_N5 N5Set NOTC_N5 N5Map RNT_N5 CFG ANC_N5.
 Import TA.
 
+(*  
+The default network has 5 nodes and is fully connected:
+0 - root node
+1 - nonroot node
+2 - nonroot node
+3 - nonroot node
+4 - nonroot node
+*)
+
+(* first set up something to aggregate for each *)
+Definition input_0_node_0 := Local (@Ordinal 128 10 erefl).
+Definition input_0_node_1 := Local (@Ordinal 128 5 erefl).
+Definition input_0_node_2 := Local (@Ordinal 128 3 erefl).
+Definition input_0_node_3 := Local (@Ordinal 128 7 erefl).
+Definition input_0_node_4 := Local (@Ordinal 128 2 erefl).
+
+(* check that root has level 0, should return (TA.LevelResponse (Some 0)) *)
+Definition input_1_node_0 := LevelRequest.
+
+(* check that root has aggregate 10, should return (TA.AggregateResponse 10) *)
+Definition input_2_node_0 := AggregateRequest.
+
+(* make root node send its level to everyone *)
+Definition input_3_node_0 := Broadcast.
+
+(* check that nonroot node 2 has level 1, should return (TA.LevelResponse (Some 1)) *)
+Definition input_1_node_2 := LevelRequest.
+
+(* make nonroot node 2 send its aggregate to root *)
+Definition input_2_node_2 := SendAggregate.
+
+(* check root aggregate, should return (TA.AggregateResponse 13) *)
+Definition input_4_node_0 := AggregateRequest.
+
 Extraction "extraction/aggregation/coq/TreeAggregation.ml" List.seq TreeAggregation_BaseParams TreeAggregation_MultiParams.
+Extraction "extraction/aggregation/coq/TreeAggregationSetup.ml" input_0_node_0 input_0_node_1 input_0_node_2 input_0_node_3 input_0_node_4 input_1_node_0 input_2_node_0 input_3_node_0 input_1_node_2 input_2_node_2 input_4_node_0.
