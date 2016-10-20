82f82f72c0dc6702e2efcdfe54bcb0b96bfcd3e3
Doug Woos <doug.woos@gmail.com>
2016-10-20 14:50:52 -0700

    StructTact-ify some proofs

--- systems/TreeStatic.v
+++ systems/TreeStatic.v
@@ -816,6 +816,45 @@ end; simpl.
   eauto.
 Qed.
 
+Ltac induct_step :=
+  match goal with
+  | H : step_ordered_failure_star _ _ _ |- _ =>
+    prep_induction H;
+    induction H using refl_trans_1n_trace_n1_ind; intros; subst;
+    [|match goal with
+      | _ : step_ordered_failure ?s _ _,
+            IH : context [?s = _] |- _ =>
+        destruct s as [failed' net'];
+        specialize (IH net' failed')
+      end]
+  end.
+
+(* bfs_net_ok_root_levels_empty *)
+Lemma Tree_root_levels_empty' :
+  forall net failed tr,
+  step_ordered_failure_star step_ordered_failure_init (failed, net) tr -> 
+  forall n, ~ In n failed -> 
+  root n ->
+  (net.(onwState) n).(levels) = NMap.empty lv.
+Proof.
+  intros. induct_step.
+  - unfold step_ordered_failure_init in *.
+    find_inversion. simpl.
+    unfold InitData.
+    now break_match.
+  - match goal with
+    | [ H : step_ordered_failure _ _ _ |- _ ] => invc H
+    end; simpl.
+    + find_apply_lem_hyp net_handlers_NetHandler.
+      net_handler_cases; auto; simpl in *;
+        update_destruct_max_simplify; repeat find_rewrite; auto.
+    + find_apply_lem_hyp input_handlers_IOHandler.
+      io_handler_cases; simpl in *; auto;
+        update_destruct_max_simplify; repeat find_rewrite; auto.
+    + intros. simpl in *.
+      eauto.
+Qed.
+
 (* bfs_net_ok_root_levels_bot *)
 Lemma Tree_root_levels_bot : 
 forall net failed tr,
@@ -823,46 +862,25 @@ forall net failed tr,
   forall n, ~ In n failed -> root n ->
   forall n', NMap.find n' (net.(onwState) n).(levels) = None.
 Proof.
-move => onet failed tr H.
-have H_eq_f: failed = fst (failed, onet) by [].
-have H_eq_o: onet = snd (failed, onet) by [].
-rewrite H_eq_f {H_eq_f}.
-rewrite {2}H_eq_o {H_eq_o}.
-remember step_ordered_failure_init as y in *.
-move: Heqy.
-induction H using refl_trans_1n_trace_n1_ind => H_init {failed}.
-  rewrite H_init /=.
-  move => n H_in H_r n'.
-  rewrite /InitData /=.
-  break_if => /=.
-  - apply NMapFacts.not_find_in_iff.
-    move => H_ins.
-    by apply NMapFacts.empty_in_iff in H_ins.
-  - by destruct root_dec.
-concludes.
-match goal with
-| [ H : step_ordered_failure _ _ _ |- _ ] => invc H
-end; simpl.
-- find_apply_lem_hyp net_handlers_NetHandler.
-  net_handler_cases => //=.
-  * by admit.
-  * by admit.
-  * by admit.
-  * by admit.
-  * by admit.
-  * by admit.
-  * by admit.
-  * by admit.
-- find_apply_lem_hyp input_handlers_IOHandler.
-  io_handler_cases => //=.
-  * by admit.
-  * by admit.
-  * by admit.
-  * by admit.
-  * by admit.
-- (* fail *)
-  by admit.
-Admitted.
+    intros. induct_step.
+  - unfold step_ordered_failure_init in *.
+    find_inversion. simpl.
+    unfold InitData.
+    apply NMapFacts.not_find_in_iff.
+    intro.
+    break_if; simpl in *; eapply NMapFacts.empty_in_iff; eauto.
+  - match goal with
+    | [ H : step_ordered_failure _ _ _ |- _ ] => invc H
+    end; simpl.
+    + find_apply_lem_hyp net_handlers_NetHandler.
+      net_handler_cases; auto; simpl in *;
+        update_destruct_max_simplify; repeat find_rewrite; auto.
+    + find_apply_lem_hyp input_handlers_IOHandler.
+      io_handler_cases; simpl in *; auto;
+        update_destruct_max_simplify; repeat find_rewrite; auto.
+    + intros. simpl in *.
+      eauto.
+Qed.
 
 (* in_after_all_fail_status *)
 Lemma Tree_in_after_all_fail_level : 
