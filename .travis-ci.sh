8cbae669a025c28dbaf270b4e29391fa5093f36e
Karl Palmskog <palmskog@gmail.com>
2016-11-08 22:03:32 -0600

    experiment with Coq via OPAM

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -1,11 +1,7 @@
 pushd ..
-  wget 'http://homes.cs.washington.edu/~jrw12/coq-8.5-build-local.tgz'
-  tar xf coq-8.5-build-local.tgz
-  export PATH=$PWD/coq-8.5/bin:$PATH
-
   opam init --yes --no-setup
   eval $(opam config env)
-  opam install ounit --yes
+  opam install coq ounit --yes
 
   git clone 'http://github.com/uwplse/StructTact'
   pushd StructTact
