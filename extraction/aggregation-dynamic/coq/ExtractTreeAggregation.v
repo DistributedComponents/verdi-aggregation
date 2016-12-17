4c63540030a1d982a340697c4db60c95dc88cecf
Karl Palmskog <palmskog@gmail.com>
2016-12-16 18:46:11 -0600

    move aggregation group definitions to deps file

--- extraction/aggregation-dynamic/coq/ExtractTreeAggregation.v
+++ extraction/aggregation-dynamic/coq/ExtractTreeAggregation.v
@@ -1,10 +1,6 @@
 Require Import Verdi.Verdi.
 Require Import Verdi.NameOverlay.
 
-Require Import AggregationDefinitions.
-Require Import TreeAux.
-Require Import TreeAggregationDynamic.
-
 Require Import StructTact.Fin.
 
 Require Import ExtrOcamlBasic.
@@ -17,6 +13,8 @@ Require Import ExtrOcamlNatIntExt.
 Require Import ExtrOcamlBool.
 Require Import ExtrOcamlList.
 Require Import ExtrOcamlFin.
+
+Require Import ExtractionDeps.
 
 Require Import mathcomp.ssreflect.ssreflect.
 
@@ -34,43 +32,6 @@ Require Import FMapList.
 Module NamesMap <: FMapInterface.S := FMapList.Make NamesOTCompat.
 
 Module TAuxNames := NameTypeTAux Names NamesOT NamesSet NamesOTCompat NamesMap.
-
-Require Import ZpCommutativeFinGroup.
-Definition num_zp := 0.
-Module NumZp : NatValue. Definition n := num_zp. End NumZp.
-Module AggregationGroup <: CommutativeFinGroup := CFG NumZp.
-Extract Inlined Constant num_zp => "65535".
-
-(*
-Require Import ZpCommutativeFinGroup.
-Require Import ProdCommutativeFinGroup.
-Module NumZp1 : NatValue. Definition n := 20. End NumZp1.
-Module ZpCFG1 : CommutativeFinGroup := CFG NumZp1.
-Module NumZp2 : NatValue. Definition n := 127. End NumZp2.
-Module ZpCFG2 : CommutativeFinGroup := CFG NumZp2.
-Module AggregationGroup : CommutativeFinGroup := ProdCFG ZpCFG1 ZpCFG2.*)
-
-(*
-Require Import ZpProdCommutativeFinGroup.
-Module NumZp1 : NatValue. Definition n := 20. End NumZp1.
-Module NumZp2 : NatValue. Definition n := 127. End NumZp2.
-Module AggregationGroup <: CommutativeFinGroup := CFG NumZp1 NumZp2.
-*)
-
-(*Require Import Bvector BDef.*)
-(*Require Import BvectorCommutativeFinGroup.*)
-
-(*
-Extract Inlined Constant Bvector => "int32".
-Extract Inlined Constant add => "(fun _ -> Int32.add)".
-Extract Inlined Constant opp => "(fun _ -> Int32.neg)".
-Extract Inlined Constant zero => "(fun _ -> 0l)".
-*)
-
-(*
-Module NumBits : NatValue. Definition n := 32. End NumBits.
-Module AggregationGroup <: CommutativeFinGroup := CFG NumBits.
-*)
 
 Module ADefNames := NameTypeADefs Names NamesOT NamesSet NamesOTCompat NamesMap AggregationGroup.
 Module TreeAggregationNames := TreeAggregation Names NamesOT NamesSet NamesOTCompat NamesMap RootNames AggregationGroup AdjacentNames TAuxNames ADefNames.
