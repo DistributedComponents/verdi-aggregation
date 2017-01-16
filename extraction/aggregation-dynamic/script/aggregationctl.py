43d1e7af2cb62a944696a03d980a5053b85cdb3d
Karl Palmskog <palmskog@gmail.com>
2017-01-16 16:22:14 -0600

    aggregationctl can send local data

--- extraction/aggregation-dynamic/script/aggregationctl.py
+++ extraction/aggregation-dynamic/script/aggregationctl.py
@@ -5,24 +5,31 @@ import sys
 import client
 
 def create_client(args):
-    return client.Client(args.hostname, int(args.port))
+    return client.Client(args.host, int(args.port))
 
 def aggregate(args):
     c = create_client(args)
     res = c.send_aggregate_request()
     print res
 
+def local(args):
+    c = create_client(args)
+    c.send_local(args.data)
+
 def main():
     parser = argparse.ArgumentParser()
 
     # global options
-    parser.add_argument('--hostname', required=True)
+    parser.add_argument('--host', required=True)
     parser.add_argument('--port', required=True)
     
     # subcommands
     subparsers = parser.add_subparsers(dest='cmd', help='commands')
 
-    status_parser = subparsers.add_parser('aggregate', help='Check aggregate of node')
+    aggregate_parser = subparsers.add_parser('aggregate', help='Check aggregate of node')
+
+    local_parser = subparsers.add_parser('local', help='Issue local data to node')
+    local_parser.add_argument('data', action='store', help='Data value')
     
     args = parser.parse_args()
     globals()[args.cmd](args)
