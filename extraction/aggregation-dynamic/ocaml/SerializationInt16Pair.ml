348e992f9ee7e1096df03e21398a6128773d975a
Karl Palmskog <palmskog@gmail.com>
2017-01-17 21:15:07 -0600

    added pair-related files

---
+++ extraction/aggregation-dynamic/ocaml/SerializationInt16Pair.ml
@@ -0,0 +1,37 @@
+open TreeAggregation
+open TreeAggregationNames
+open Util
+
+let serializeLevelOption olv : string =
+  match olv with
+  | Some lv -> string_of_int lv
+  | _ -> ""
+
+let deserializeInput (s : string) (c : string) : coq_Input option =
+  match s with
+  | "SendAggregate" -> Some SendAggregate
+  | "Broadcast" -> Some Broadcast
+  | "AggregateRequest" -> Some (AggregateRequest (char_list_of_string c))
+  | "LevelRequest" -> Some (LevelRequest (char_list_of_string c))
+  | _ -> 
+    try Scanf.sscanf s "Local %d %d" (fun x y -> Some (Local (Obj.magic (Obj.magic x, Obj.magic y))))
+    with _ -> None
+
+let two_compl x = if x >= 32768 then -(65536-x) else x
+
+let serializeOutput : coq_Output -> string * string = function
+  | AggregateResponse (c, x) -> (string_of_char_list c, Printf.sprintf "AggregateResponse %d %d" (two_compl (fst (Obj.magic x))) (two_compl (snd (Obj.magic x))))
+  | LevelResponse (c, olv) -> (string_of_char_list c, Printf.sprintf "LevelResponse %s" (serializeLevelOption olv))
+
+let debugSerializeInput : coq_Input -> string = function
+  | SendAggregate -> "SendAggregate"
+  | Broadcast -> "Broadcast"
+  | AggregateRequest x -> Printf.sprintf "AggregateRequest %s" (string_of_char_list x)
+  | Local x -> Printf.sprintf "Local %d %d" (two_compl (fst (Obj.magic x))) (two_compl (snd (Obj.magic x)))
+  | LevelRequest x -> Printf.sprintf "LevelRequest %s" (string_of_char_list x)
+
+let debugSerializeMsg : coq_Msg -> string = function
+  | New -> "New"
+  | Aggregate x -> Printf.sprintf "Aggregate %d %d" (two_compl (fst (Obj.magic x))) (two_compl (snd (Obj.magic x)))
+  | Fail -> "Fail"
+  | Level olv -> Printf.sprintf "Level %s" (serializeLevelOption olv)
---
+++ extraction/aggregation-dynamic/script/client-pair.py
@@ -0,0 +1,74 @@
+import socket
+import re
+import uuid
+from struct import pack, unpack
+from select import select
+
+def poll(sock, timeout):
+    return sock in select([sock], [], [], timeout)[0]
+
+class SendError(Exception):
+    pass
+
+class ReceiveError(Exception):
+    pass
+
+class Client(object):
+    re_aggregate_response = re.compile(r'AggregateResponse\W+([0-9]+)\W+([0-9]+)')
+    re_level_response = re.compile(r'LevelResponse\W+([0-9]+|-)')
+
+    def __init__(self, host, port, sock=None):
+        if not sock:
+            self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.sock.connect((host, port))
+        else:
+            self.sock = sock
+
+    def send_msg(self, msg):
+        n = self.sock.send(pack("<I", len(msg)))
+        if n < 4:
+            raise SendError
+        else:
+            self.sock.send(msg)
+
+    def recv_msg(self, re):
+        len_bytes = self.sock.recv(4)
+        if len_bytes == '':
+            raise ReceiveError
+        else:
+            len = unpack("<I", len_bytes)[0]
+            data = self.sock.recv(len)
+            if data == '':
+                raise ReceiveError
+            else:
+                return self.parse_response(data, re)
+
+    def send_local(self, local1, local2):
+        self.send_msg('Local' + ' ' + str(local1) + ' ' + str(local2))
+
+    def send_send_aggregate(self):
+        self.send_msg('SendAggregate')
+
+    def send_broadcast(self):
+        self.send_msg('Broadcast')
+               
+    def send_aggregate_request(self):
+        self.send_msg('AggregateRequest')
+        return self.recv_msg(self.re_aggregate_response)
+
+    def send_level_request(self):
+        self.send_msg('LevelRequest')
+        return self.recv_msg(self.re_level_response)
+
+    def deserialize(self, data):
+        if data == '-':
+            return None
+        return data
+
+    def parse_response(self, data, re):
+        try:
+            match = re.match(data)
+            return [self.deserialize(match.group(n)) for n in (1,2)]
+        except Exception as e:
+            print "Parse error, data=%s" % data
+            raise e
