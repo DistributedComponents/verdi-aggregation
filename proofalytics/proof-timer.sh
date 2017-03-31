c40419794b9977e10b9bc6912a1285d06b19b642
Karl Palmskog <palmskog@gmail.com>
2017-03-31 12:29:48 -0500

    don't pipe proofalytics stdout to /dev/null to prevent early travis termination

--- proofalytics/proof-timer.sh
+++ proofalytics/proof-timer.sh
@@ -14,7 +14,7 @@ SEP="__PROOFALYTICS__"
 SANDBOX="$(mktemp -d "${PADIR}/../../proofalytics-tmp-XXXXX")"
 cp -R "${PADIR}/.." "${SANDBOX}/"
 
-pushd "$SANDBOX" > /dev/null
+pushd "$SANDBOX"
   # annotate proofs
   for v in $(find . -name '*.v'); do
     scratch="$(mktemp "proot-time-annot-tmp-XXXXX")"
@@ -41,7 +41,7 @@ pushd "$SANDBOX" > /dev/null
     | awk -W lint=fatal -f "${PADIR}/proof-times-csv.awk" \
     | awk -W lint=fatal -v key=2 -f "${PADIR}/csv-sort.awk" \
     > "$PROOF_TIMES"
-popd > /dev/null
+popd
 
 # clean up
 rm -rf "$SANDBOX"
