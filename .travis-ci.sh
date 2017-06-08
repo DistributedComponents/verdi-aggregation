3be649dc98022ef4ed2eae06a9c0b0ed59c82595
Karl Palmskog <palmskog@gmail.com>
2017-06-08 18:06:40 -0500

    new approach to proofalytics

--- .travis-ci.sh
+++ .travis-ci.sh
@@ -10,7 +10,10 @@ opam pin add coq $COQ_VERSION --yes --verbose
 
 case $MODE in
   proofalytics)
-    opam pin add verdi-aggregation-proofalytics . --yes --verbose &
+    opam pin add verdi-aggregation . --yes --verbose --no-action
+    opam install verdi-aggregation --yes --verbose --deps-only
+    ./configure
+    make proofalytics &
     # Output to the screen every 9 minutes to prevent a travis timeout
     export PID=$!
     while [[ `ps -p $PID | tail -n +2` ]]; do
